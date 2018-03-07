f0422c96343d447482588eab0e543bc7ee9d78c5
Cayle Sharrock <cayle@nimbustech.biz>
2018-03-07 15:56:31 +0200

    Add some book endpoints to swagger definition

--- scripts/build.js
+++ scripts/build.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-var Path = require('path');
+const Path = require('path');
 
 require('shelljs/global');
 set('-e');
 
-mkdir('-p', 'web_deploy')
+mkdir('-p', 'web_deploy');
 
 cp('-R', 'web/*', 'web_deploy/');
 
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
+const SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
+rm('-rf', 'web_deploy/swagger-ui/');
+cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/');
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
 
